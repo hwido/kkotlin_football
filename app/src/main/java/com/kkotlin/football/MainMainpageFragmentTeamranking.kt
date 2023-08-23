bf9bf58975ebef52266e1aebaee620a4995c87af
hwido <hyudojung@naver.com>
2023-08-23 12:10:43 +0900

    fragment change

---
+++ app/src/main/java/com/kkotlin/football/MainMainpageFragmentTeamranking.kt
@@ -0,0 +1,14 @@
+package com.kkotlin.football
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.hwido.football.R
+
+class MainMainpageFragmentTeamranking : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.main_mainpage_fragment_teamranking, container, false)
+    }
+}
